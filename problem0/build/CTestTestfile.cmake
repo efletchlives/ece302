e2f56ec052c151ba0ee7237b259276c96f6f3f70
Evan Fletcher <ehf21@pitt.edu>
2025-01-14 19:55:37 -0500

    commit 1

---
+++ problem0/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/efletchlives/VsCode/ECE302/ECE302_EHF/problem0
+# Build directory: /home/efletchlives/VsCode/ECE302/ECE302_EHF/problem0/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(test-add "test-add")
+set_tests_properties(test-add PROPERTIES  _BACKTRACE_TRIPLES "/home/efletchlives/VsCode/ECE302/ECE302_EHF/problem0/CMakeLists.txt;17;add_test;/home/efletchlives/VsCode/ECE302/ECE302_EHF/problem0/CMakeLists.txt;0;")
